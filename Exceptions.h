596b1df65298e84e90b8c61e46a3acbe6e423869
Atif Aziz <code@raboof.com>
2011-12-21 23:26:20 +0100

    Copyright year

--- Exceptions.h
+++ Exceptions.h
@@ -1,5 +1,5 @@
 // FINDPATH - Locates a file using the Windows search path
-// Copyright (C) 2004, Atif Aziz http://www.raboof.com
+// Copyright (C) 2002, Atif Aziz http://www.raboof.com
 //
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
